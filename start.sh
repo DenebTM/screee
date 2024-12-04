e690c794c4f8036615b9b997747edb2533814834
DenebTM <deneb@screee.ee>
2024-12-04 21:45:17 +0100

    switch from nginx to caddy
    for img src, use random filename instead of static url with random_index

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --rm --name screee \
 	-p 3100:80 \
-	-v ./nginx.conf:/etc/nginx/nginx.conf \
-	-v ./content:/usr/share/nginx/html \
-	nginx:latest
+	-v ./Caddyfile:/etc/caddy/Caddyfile \
+	-v ./content:/usr/share/caddy \
+docker.io/caddy:latest
